608cddac70175583777993097e4be402396f0e76
sahilnarwal2111 <500097751@stu.upes.ac.in>
2023-04-04 13:55:05 +0530

    first commit

---
+++ src/main/java/MyPkg/MyCalc.java
@@ -0,0 +1,14 @@
+package MyPkg;
+
+public class MyCalc {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println(c.addition(100,5));
+		System.out.println(c.addition(100,5));
+		System.out.println(c.addition(100,5));
+		System.out.println(c.addition(100,5));
+		
+	}
+
+}
